d8e3b21d9c602bfb4208fc436beed1e7049984f9
Himanshu Gupta <guptasitapur489@gmail.com>
2023-11-15 11:10:42 +0530

    Completed sign-in route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from '../models/user.model.js';
 import bcryptjs from 'bcryptjs'
 import {errorHandler} from '../utils/error.js'
+import jwt from 'jsonwebtoken'
 
 const hash_password_times = 10;
 export const signup = async (req , res, next)=>{
@@ -17,3 +18,23 @@ export const signup = async (req , res, next)=>{
         next(error);
     }
 }
+
+export const signin = async (req , res, next)=>{
+    const {email, password} = req.body;
+    if(!email || !password){
+        res.status(400).json({"Message":"Please provide username and password"})
+    }
+    let user ;
+    try{
+        const validUser = await User.findOne({ email });
+        if (!validUser) return next(errorHandler(404 , 'User Not found'));
+        const validPassword = bcryptjs.compareSync(password, validUser.password);
+        if (!validPassword) return next(errorHandler(401, "Invalid Credentials!"));
+        
+        const token = jwt.sign({id:validUser._id}, process.env.JWT_SECRET);
+        const {password:pass, ...rest} = validUser._doc;
+        res.cookie('access_token', token , {httpOnly:true}).status(200).json(rest);
+    }catch(err){
+        next(err)
+    }
+};
