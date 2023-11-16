e3696034ff63b973c790b8e791f9b4c36986d230
Himanshu Gupta <guptasitapur489@gmail.com>
2023-11-16 19:30:20 +0530

    Completed signout functionality

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -72,3 +72,12 @@ export const google = async (req , res, next)=>{
         next(error);
     }
 }
+
+export const signout = async (req, res, next) => {
+  try {
+    res.clearCookie("access_token");
+    res.status(200).json("User has been logged out successfull");
+  } catch (error) {
+    next(errorHandler(401, error));
+  }
+};
--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -14,6 +14,9 @@ import {
   deleteUserFailure,
   deleteUserSuccess,
   deleteUserStart,
+  signOutUserStart,
+  signOutUserSuccess,
+  signOutUserFailure,
 } from "../redux/user/userSlice.js";
 import { useDispatch } from "react-redux";
 // firebase storage rules-
@@ -103,6 +106,20 @@ export default function Profile() {
       dispatch(deleteUserFailure(error.message));
     }
   }
+  const handleSignOut = async () => {
+    try {
+      dispatch(signOutUserStart())
+      const res = await fetch('/api/auth/signout');
+      const data = await res.json();
+      if(data.success == false) {
+        dispatch(signOutUserFailure(data.message));
+        return ;
+      }
+      dispatch(signOutUserSuccess(data));
+    } catch (error) {
+      dispatch(signOutUserFailure(data.message));
+    }
+  };
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
@@ -169,11 +186,16 @@ export default function Profile() {
         </button>
       </form>
       <div className="flex justify-between mt-4">
-        <span onClick={handleDeleteUser} className="text-red-700 cursor-pointer">
-
+        <span
+          onClick={handleDeleteUser}
+          className="text-red-700 cursor-pointer"
+        >
           Delete Account
         </span>
-        <span className="text-red-700 cursor-pointer"> Sign Out</span>
+        <span onClick={handleSignOut} className="text-red-700 cursor-pointer">
+          {" "}
+          Sign Out
+        </span>
       </div>
       <p className="text-red-700 mt-3">{error ? error : ""}</p>
       <p className="text-green-700 mt-3">
