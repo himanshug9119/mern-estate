b47f774e2f4282bbad685308be7b96759c2a2b24
Himanshu Gupta <guptasitapur489@gmail.com>
2023-11-16 18:30:55 +0530

    removed 1 from update query

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -56,7 +56,7 @@ export default function Profile() {
     e.preventDefault();
     try {
       dispatch(updateUserStart());
-      const res = await fetch(`/api/user/update/${currentUser._id}1` , {
+      const res = await fetch(`/api/user/update/${currentUser._id}` , {
         method:"POST",
         headers:{
           "Content-Type":"application/json",
