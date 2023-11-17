2abf87b35d200d263502b94f3948e3d7b9e4c763
Himanshu Gupta <guptasitapur489@gmail.com>
2023-11-17 10:31:27 +0530

    If user is logged In then user can'nt go to signin or signup page and vice-verse

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,18 +5,24 @@ import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 import Profile from './pages/Profile';
 import Header from './components/Header';
-import PrivateRoute from './components/PrivateRoute';
+import { PrivateRoute1, PrivateRoute2 } from "./components/PrivateRoute";
 export default function App(){
-  return <BrowserRouter>
-    <Header/>
-    <Routes>
-      <Route path="/" element={<Home/>} />
-      <Route path="/sign-in" element={<SignIn/>} />
-      <Route path="/sign-up" element={<SignUp/>} />
-      <Route path="/about" element={<About/>} />
-      <Route element={<PrivateRoute/>}>
-        <Route path="/profile" element={<Profile/>} />
-      </Route>
-    </Routes>
-  </BrowserRouter>
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route element={<PrivateRoute1 />}>
+          <Route path="/profile" element={<Profile />} />
+        </Route>
+        <Route element={<PrivateRoute2 />}>
+          <Route path="/sign-in" element={<SignIn />} />
+        </Route>
+        <Route element={<PrivateRoute2 />}>
+          <Route path="/sign-up" element={<SignUp />} />
+        </Route>
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
--- client/src/components/PrivateRoute.jsx
+++ client/src/components/PrivateRoute.jsx
@@ -1,7 +1,11 @@
 import {useSelector} from 'react-redux'
 import {Outlet, Navigate} from 'react-router-dom'
 
-export default function PrivateRoute() {
+export function PrivateRoute1() {
     const {currentUser} = useSelector((state) =>state.user)
     return currentUser ? <Outlet/> : <Navigate to='/sign-in'/>
+}
+export function PrivateRoute2() {
+    const {currentUser} = useSelector((state) =>state.user)
+    return currentUser ? <Navigate to='/profile' /> : <Outlet/>
 }
