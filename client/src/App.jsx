f3532609a20f8c4361d3163eb2255d37d8794b2d
Himanshu Gupta <guptasitapur489@gmail.com>
2023-11-24 00:54:53 +0530

    Comment out createList page from app.jsx file

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,7 +6,7 @@ import SignUp from './pages/SignUp';
 import Profile from './pages/Profile';
 import Header from './components/Header';
 import Listing from './pages/Listing';
-// import CreateListing from './pages/CreateListing';
+import CreateListing from './pages/CreateListing';
 import UpdateListing from './pages/UpdateListing';
 import Search from './pages/Search';
 import { PrivateRoute1, PrivateRoute2 } from './components/PrivateRoute';
@@ -18,7 +18,7 @@ export default function App(){
         <Route path="/" element={<Home />} />
         <Route element={<PrivateRoute1 />}>
           <Route path="/profile" element={<Profile />} />
-          {/* <Route path="/create-listing" element={<CreateListing />} /> */}
+          <Route path="/create-listing" element={<CreateListing />} />
           <Route path="/update-listing/:listingId" element={<UpdateListing />} />
         </Route>
         <Route element={<PrivateRoute2 />}>
