b342c732dffda15d16a57576c9c85819153a0f87
Himanshu Gupta <guptasitapur489@gmail.com>
2024-06-21 11:58:31 +0530

    Updated Footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import {Link, useLocation, useNavigate} from 'react-router-dom'
+import '@fortawesome/fontawesome-free/css/all.min.css';
 const Footer = () => {
   return (
-    <footer className="bg-gray-200 h-screen md:h-48 py-12 px-4 md:px-8">
-      <div className="container mx-auto flex flex-wrap justify-between">
-        <div className="w-full md:w-1/2 xl:w-1/3 mb-4 md:mb-0">
+    <footer className="bg-gray-200 h-screen mt-20 md:h-auto py-12 px-4 md:px-8">
+      <div className="container mx-auto flex flex-wrap justify-between ">
+        <div className="w-full md:w-1/2 xl:w-1/3 mb-4 sm:mb-5">
           <h5 className="uppercase text-lg mb-2">Quick Links</h5>
           <ul className="list-none mb-0">
             <li className="mb-2">
@@ -26,24 +27,19 @@ const Footer = () => {
         </div>
         <div className="w-full md:w-1/2 xl:w-1/3 mb-4 md:mb-0">
           <h5 className="uppercase text-lg mb-2">Social Media</h5>
-          <ul className="list-none mb-0 flex justify-center md:justify-start">
+          <ul className="list-none mb-0 flex justify-start">
             <li className="mr-4">
-              <a href="#" className="text-gray-600 hover:text-gray-900">
-                <i className="fab fa-facebook-f" />
-              </a>
-            </li>
-            <li className="mr-4">
-              <a href="#" className="text-gray-600 hover:text-gray-900">
+              <a href="https://x.com/himanshug9119" className="text-gray-600 hover:text-gray-900" target='_blank'>
                 <i className="fab fa-twitter" />
               </a>
             </li>
             <li className="mr-4">
-              <a href="#" className="text-gray-600 hover:text-gray-900">
+              <a href="https://www.instagram.com/himanshug9119/" className="text-gray-600 hover:text-gray-900" target='_blank'>
                 <i className="fab fa-instagram" />
               </a>
             </li>
             <li>
-              <a href="#" className="text-gray-600 hover:text-gray-900">
+              <a href="https://www.linkedin.com/in/himanshug9119/" className="text-gray-600 hover:text-gray-900" target='_blank'>
                 <i className="fab fa-linkedin-in" />
               </a>
             </li>
@@ -51,7 +47,7 @@ const Footer = () => {
         </div>
         <div className="w-full md:w-1/2 xl:w-1/3 mb-4 md:mb-0">
           <h5 className="uppercase text-lg mb-2">Subscribe to our Newsletter</h5>
-          <form className="flex flex-wrap">
+          <form className="flex flex-wrap gap-2">
             <input
               type="email"
               placeholder="Enter your email address"
@@ -63,8 +59,8 @@ const Footer = () => {
           </form>
         </div>
       </div>
-      <p className="text-center text-gray-600 text-sm">
-        &copy; 2023 Real Estate Listing Project. All rights reserved.
+      <p className="text-center text-gray-600 text-sm mb-2">
+        &copy; 2024 Real Estate Listing Project. All rights reserved.
       </p>
     </footer>
   );
