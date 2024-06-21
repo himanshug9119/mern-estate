1368a7f59f276285d1222c0f461be2c7ca0d2e0f
Himanshu Gupta <guptasitapur489@gmail.com>
2024-06-21 12:39:55 +0530

    Updated verify user from utils

--- api/utils/verifyUser.js
+++ api/utils/verifyUser.js
@@ -8,8 +8,7 @@ export const verifyToken = (req, res, next) =>{
     const token = req.cookies.access_token;
     if(!token) return next(errorHandler(401, 'Unauthorized'))
     jwt.verify(token, JWT_SECRET, (err, user) => {
-  if (err) return next(errorHandler(403, "Forbidden"));
-      console.log(token);
+      if (err) return next(errorHandler(403, "Forbidden"));
       req.user = user;
       next();
     });
