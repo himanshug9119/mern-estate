619977b214b7de6ea58851a80b15a7c76b2f329a
Himanshu Gupta <guptasitapur489@gmail.com>
2024-10-19 02:49:06 +0530

    Listing are visible no error

--- client/src/components/ShowListings.jsx
+++ client/src/components/ShowListings.jsx
@@ -1,24 +1,29 @@
 import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
+import Loader from "./Loader";
+
+
 
 export default function ShowListings() {
   const { currentUser } = useSelector((state) => state.user);
   const [userListings, setUserListings] = useState([]);
   const [showListingsError, setShowListingsError] = useState(false);
-
+  const [fetchingListing, setFetchingListing] = useState(false);
   useEffect(() => {
     const fetchUserListings = async () => {
       try {
+        setShowListingsError(false);
+        setFetchingListing(true);
         const res = await fetch(`/api/user/listings/${currentUser._id}`);
         const data = await res.json();
-
-        if (data.success === false) {
+        setFetchingListing(false);
+        if (data.success == false) {
           setShowListingsError(true);
           return;
         }
 
-        setUserListings(data.listings);
+        setUserListings(data);
       } catch (error) {
         setShowListingsError(true);
       }
@@ -37,17 +42,17 @@ export default function ShowListings() {
       });
 
       const data = await res.json();
-      if (data.success === false) {
+      if (!data.success) {
         console.log(data.message);
         return;
       }
 
       // Refresh listings after deletion
+      alert("Listing Deleted Successfully");
       setUserListings((prevListings) =>
         prevListings.filter((listing) => listing._id !== listingId)
       );
     } catch (error) {
-      console.log(error);
       setShowListingsError(true);
     }
   };
@@ -57,43 +62,57 @@ export default function ShowListings() {
       <h1 className="text-xl md:text-2xl font-semibold text-center mb-4">
         Your Listings
       </h1>
-      {showListingsError && (
-        <p className="text-red-600">Error fetching listings</p>
-      )}
-      {userListings.length === 0 && (
-        <p className="text-red-600">No Listings found</p>
-      )}
-      {userListings.length > 0 && (
-        <div className="flex flex-col gap-4">
-          {userListings.map((listing) => (
-            <div
-              key={listing._id}
-              className="flex border rounded-lg p-2 md:p-4 justify-between items-center gap-3 shadow-md hover:shadow-lg transition"
-            >
-              <Link to={`/listing/${listing._id}`}>
-                <img
-                  className="h-14 w-14 md:h-16 md:w-16 object-cover rounded-md"
-                  src={listing.imageUrls[0]}
-                  alt="Listing Cover"
-                />
-              </Link>
-              <Link
-                className="flex-1 font-semibold hover:underline truncate"
-                to={`/listing/${listing._id}`}
+      {fetchingListing && <Loader />}
+
+      {!fetchingListing &&
+        !showListingsError &&
+        userListings &&
+        userListings.length === 0 && (
+          <p className="text-red-600">No Listings found</p>
+        )}
+      {!fetchingListing &&
+        !showListingsError &&
+        userListings &&
+        userListings.length > 0 && (
+          <div className="flex flex-col gap-4">
+            {userListings.map((listing) => (
+              <div
+                key={listing._id}
+                className="flex border rounded-lg p-2 md:p-4 justify-between items-center gap-3 shadow-md hover:shadow-lg transition"
               >
-                <p>{listing.name}</p>
-              </Link>
-              <div className="flex flex-col gap-1 items-center">
-                <button
-                  onClick={() => handleDeleteListing(listing._id)}
-                  className="text-red-600 uppercase hover:underline"
+                <Link to={`/listing/${listing._id}`}>
+                  <img
+                    className="h-14 w-14 md:h-16 md:w-16 object-cover rounded-md"
+                    src={listing.imageUrls[0]}
+                    alt="Listing Cover"
+                  />
+                </Link>
+                <Link
+                  className="flex-1 font-semibold hover:underline truncate"
+                  to={`/listing/${listing._id}`}
                 >
-                  Delete
-                </button>
+                  <p>{listing.name}</p>
+                </Link>
+                <div className="flex flex-col gap-1 items-center">
+                  <button
+                    onClick={() => handleDeleteListing(listing._id)}
+                    className="text-red-600 uppercase hover:underline"
+                  >
+                    Delete
+                  </button>
+                  <Link to={`/update-listing/${listing._id}`}>
+                    {" "}
+                    <button className="text-green-600 uppercase hover:underline transition">
+                      Edit{" "}
+                    </button>{" "}
+                  </Link>
+                </div>
               </div>
-            </div>
-          ))}
-        </div>
+            ))}
+          </div>
+        )}
+      {!fetchingListing && showListingsError && (
+        <p className="text-red-600">Error fetching listings</p>
       )}
     </div>
   );
